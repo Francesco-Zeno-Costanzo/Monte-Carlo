edda1e604bc194757f1d6d5c27b15cbde3d7342d
Francesco Zeno Costanzo <zenofrancesco99@gmail.com>
2021-03-20 00:44:56 +0100

    Add files via upload

---
+++ Metropolis.py
@@ -0,0 +1,42 @@
+import numpy as np
+def GEN(a,c,M,r0,n=1):
+    if n==1:
+        return np.mod(a*r0+c,M)
+    else:
+        r=np.zeros(n)
+        r[0]=r0
+        for i in range(1,n):
+            r[i]=np.mod(a*r[i-1]+c,M)
+    return r
+
+##
+import random as rn
+import scipy.integrate as si
+import matplotlib.pyplot as plt
+def p(x):
+    return 2*np.exp(-(x)**2)+np.exp(-(x+3)**2)+np.exp(-(x-3)**2)
+
+
+delta=5
+x=np.array([0.])
+N=5000
+naccept=0
+counter=0
+t=np.linspace(-5, 5, N+1)
+Norm=si.simps(p(t), t, dx=1/len(t), even='first' )
+
+while naccept < N:
+    counter+=1
+    xt=x[-1]+delta*(2*rn.random()-1)
+    w=p(xt)/p(x[-1])
+    r=rn.random()
+    if w > r:
+        x=np.insert(x, len(x), xt)
+        naccept+=1
+
+print( naccept/counter)
+plt.title('Metropolis')
+plt.grid()
+plt.hist(x, 100,  histtype = 'step', density=True)
+plt.plot(t, p(t)/Norm)
+plt.show()
